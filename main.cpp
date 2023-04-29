30a86cda6e42b14f7072a356d7395c4b6c8090ad
abdullahkhalid7 <abdullahkhalid237@gmail.com>
2023-04-29 07:00:30 +0500

    29/Apr/2023 - 1:00 PM

--- main.cpp
+++ main.cpp
@@ -2,9 +2,43 @@
 
 using namespace std;
 
+/*
+1. admin 
+2. staff
+3. login 
+4. salaries/ employees data
+5. add remove employees
+*/
+
+void admin()
+{
+    cout << "admin" << endl;
+}
+
+void staff()
+{
+    cout << "staff" << endl;
+}
+
+void login()
+{
+    cout << "login" << endl;
+}
+
+void salaries()
+{
+    cout << "salaries" << endl;
+}
+
+void add_remove_employees()
+{
+    cout << "add_remove_employees" << endl;
+}
+
 int main()
 {
     cout << "Hello World!" << endl;
     return 0;
+    
 }
 
